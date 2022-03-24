85ff69a7582091ce3c1efdc3b7b36ae477587b50
mcalle1@eafit.edu.co <mcalle1@eafit.edu.co>
2022-03-23 23:57:20 -0500

    feat: commands and prefix

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
+const prefix = "!";
 require('dotenv').config()
 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.on('messageCreate', message => {
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	if (!client.commands.has(commandName)) return;
+	const command = client.commands.get(commandName);
+  
+	try {
+		command.execute(message, args);
+	}
+	 catch (error) {
+		  console.error(error);
+		message.reply('Error!');
+	  }
+  
+  });
+
+
 
 client.login(process.env.TOKEN);
